18a10facd2c581929e0728d235dd614b805e58f4
sadkitties <sadkitties@users.noreply.github.com>
2020-01-27 16:09:12 +0100

    files to be used later for practice of machile learning

--- src/titanic.py
+++ src/titanic.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 # data visualization
 import seaborn as sns
-"""%matplotlib inline"""
+"""%matplotlib inline""" 
 from matplotlib import pyplot as plt
 from matplotlib import style
 
@@ -27,4 +27,33 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC, LinearSVC
 from sklearn.naive_bayes import GaussianNB
-test_df = pd.read_csv("/test.csv)
+test_df = pd.read_csv("..\\data\\processed\\test.csv")
+train_df = pd.read_csv("..\\data\\processed\\train.csv")
+survived = 'survived'
+not_survived = 'not survived'
+fig, axes = plt.subplots(nrows=1, ncols=2,figsize=(10, 4))
+women = train_df[train_df['Sex']=='female']
+men = train_df[train_df['Sex']=='male']
+ax = sns.distplot(women[women['Survived']==1].Age.dropna(), bins=18, label = survived, ax = axes[0], kde =False)
+ax = sns.distplot(women[women['Survived']==0].Age.dropna(), bins=40, label = not_survived, ax = axes[0], kde =False)
+ax.legend()
+ax.set_title('Female')
+ax = sns.distplot(men[men['Survived']==1].Age.dropna(), bins=18, label = survived, ax = axes[1], kde = False)
+ax = sns.distplot(men[men['Survived']==0].Age.dropna(), bins=40, label = not_survived, ax = axes[1], kde = False)
+ax.legend()
+_ = ax.set_title('Male')
+"""Data visualization, this is a saved fig of the above visualization
+plt.savefig('male and female.png') 
+reproduce visualization and compare if necessary"""
+
+
+
+
+
+
+
+
+
+
+
+
